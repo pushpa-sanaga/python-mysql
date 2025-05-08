d578135174241c1fd471eb5da04322bb5f9c23a9
pushpa-sanaga <pushpa.sanaga22@gmail.com>
2025-05-08 15:57:57 +0530

    i am commiting 2.py

---
+++ 2.py
@@ -0,0 +1,20 @@
+a=2>3
+print(a)
+print(type(a))
+a="welcome to python"
+print(a)
+x="hello"
+def fun():
+    x="pushpa"
+    print(x)
+fun()
+print(type(x),x)
+s=20+2j
+print(type(s))
+w=34.6
+print(type(w))
+r=45
+print(type(r))
+my1name="pushpa"
+print(my_name)
+print(type(my_name))
